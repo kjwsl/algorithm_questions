71704d0984bc9b6efef90b4e90414b5cf395704e
kjwsl <kjwdev01@gmail.com>
2024-10-14 23:03:20 +0900

    solve p2 part 2

--- p2/src/main.rs
+++ p2/src/main.rs
@@ -28,14 +28,13 @@ fn extract_beads(text: &str) -> Option<Color> {
     }
 }
 
-fn solve(input: Vec<String>, threshold: (u32, u32, u32)) -> u32 {
+fn solve(input: Vec<String>, threshold: (u32, u32, u32), part: u8) -> u32 {
     let mut sum = 0;
     for line in input {
         let line = line;
         let parts: Vec<&str> = line.split(":").collect();
 
         if parts.len() != 2 {
-            println!("Invalid line: {}", line);
             continue;
         }
 
@@ -45,60 +44,68 @@ fn solve(input: Vec<String>, threshold: (u32, u32, u32)) -> u32 {
         }
 
         let iterations = parts[1].split(";").collect::<Vec<&str>>();
-        println!("Iterations: {:?}", iterations);
 
+        let mut min_rgb = (0, 0, 0);
         let mut is_valid: bool = true;
         for it in iterations {
             let rgb: Vec<&str> = it.split(",").collect();
-            println!("RGB: {:?}", rgb);
             for i in 0..rgb.len() {
                 let bead = extract_beads(rgb[i]);
                 if let Some(bead) = bead {
                     match bead {
                         Color::Red(num) => {
-                            if num > threshold.0 {
+                            if part == 1 && num > threshold.0 {
                                 is_valid = false;
                                 break;
                             }
+                            min_rgb.0 = std::cmp::max(min_rgb.0, num);
                         }
                         Color::Green(num) => {
-                            if num > threshold.1 {
+                            if part == 1 && num > threshold.1 {
                                 is_valid = false;
                                 break;
                             }
+                            min_rgb.1 = std::cmp::max(min_rgb.1, num);
                         }
                         Color::Blue(num) => {
-                            if num > threshold.2 {
+                            if part == 1 && num > threshold.2 {
                                 is_valid = false;
                                 break;
                             }
+                            min_rgb.2 = std::cmp::max(min_rgb.2, num);
                         }
                     }
                 } else {
-                    println!("Invalid bead: {}", rgb[i]);
                     is_valid = false;
                     break;
                 }
             }
         }
-        if is_valid {
+        if part == 1 && is_valid {
             sum += game_num;
+        } else if part == 2 {
+            sum += min_rgb.0 * min_rgb.1 * min_rgb.2;
         }
-        println!("Game number: {}", game_num);
-        println!("Sum: {}", sum);
     }
-    sum
+    return sum;
 }
 
 fn main() -> io::Result<()> {
-    let sample_path = Path::new("sample.txt");
-    let file = File::open(&sample_path)?;
+    let path = Path::new("sample1.txt");
+    let file = File::open(&path)?;
     let reader = io::BufReader::new(file);
     // Red, Green, Blue
     let threshold = (12, 13, 14);
     let lines: Vec<String> = reader.lines().collect::<Result<_, _>>()?;
-    let ans = solve(lines, threshold);
-    println!("Answer: {}", ans);
+    let ans = solve(lines, threshold, 1);
+    println!("Answer for Part 1!!: {}", ans);
+
+    let path = Path::new("sample2.txt");
+    let file = File::open(&path)?;
+    let reader = io::BufReader::new(file);
+    let lines: Vec<String> = reader.lines().collect::<Result<_, _>>()?;
+    let ans = solve(lines, threshold, 2);
+    println!("Answer for Part 2!!: {}", ans);
 
     Ok(())
 }
