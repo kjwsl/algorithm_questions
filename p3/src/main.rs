46896349f07bf009eebdf834bbe5dfd0b2d0d634
kjwsl <kjwdev01@gmail.com>
2024-11-01 18:03:53 +0900

    update: solve p3 part 1

--- p3/src/main.rs
+++ p3/src/main.rs
@@ -1,78 +1,108 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
-use std::slice::Windows;
+use std::ops::Range;
 
-struct NumString {
-    start: usize,
-    end: usize,
-    number: String,
+#[inline]
+fn is_symbol(c: char) -> bool {
+    c != '.' && !c.is_digit(10)
 }
 
-impl NumString {
-    fn new(start: usize, end: usize, number: String) -> NumString {
-        NumString { start, end, number }
-    }
-}
+fn get_number_ranges(line: &str) -> Vec<Range<usize>> {
+    let mut ranges = Vec::new();
+    let mut start = 0;
+    let mut in_range = false;
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
-
-fn find_next_num(text: &str, pos: usize) -> NumString {
-    let mut start = pos;
-    let mut end = pos;
-    let mut found = false;
-    let mut number = String::new();
-    text.chars().into_iter().for_each(|c| {
+    for (i, c) in line.chars().enumerate() {
         if c.is_digit(10) {
-            if !found {
-                start = pos;
-                found = true;
-                number.push(c);
+            if !in_range {
+                start = i;
+                in_range = true;
             }
         } else {
-            if found {
-                end = pos - 1;
-                return;
+            if in_range {
+                ranges.push(start..i);
+                in_range = false;
             }
-        }
-    });
-    NumString::new(start, end, number)
-}
-
-fn is_part_number(lines: &Vec<String>, num: &NumString, line_num: usize) -> bool {
-    let first = (( line_num - 1 ).max(0), )
-    
-}
-
-fn solve_p1(lines: &Vec<String>) -> u32 {
-    let mut sum = 0u32;
-    for i in 0..lines.len() {
-        let line = &lines[i];
-        let mut pos = 0;
-        while pos < line.len() {
-            let num = find_next_num(line, pos);
-            if is_part_number(lines, &num, i) {
-                sum += num.number.parse::<u32>().unwrap();
-            }
-            pos = num.end + 1;
         }
     }
+
+    if in_range {
+        ranges.push(start..line.len());
+    }
+
+    ranges
+}
+
+fn is_part_number(lines: &[&str], curr_line_idx: usize, range: &Range<usize>) -> bool {
+    let start_line_idx = if curr_line_idx > 0 {
+        curr_line_idx - 1
+    } else {
+        0
+    };
+    let end_line_idx = if curr_line_idx < lines.len() - 1 {
+        curr_line_idx + 1
+    } else {
+        lines.len() - 1
+    };
+
+    let start_col_idx = if range.start > 0 { range.start - 1 } else { 0 };
+    let end_col_idx = if range.end < lines[curr_line_idx].len() - 1 {
+        range.end
+    } else {
+        lines[curr_line_idx].len() - 1
+    };
+
+    for row_idx in start_line_idx..=end_line_idx {
+        let line = &lines[row_idx];
+        if row_idx == curr_line_idx {
+            let start_char = line.chars().nth(start_col_idx).unwrap();
+            let end_char = line.chars().nth(end_col_idx).unwrap();
+            if is_symbol(start_char) || is_symbol(end_char) {
+                return true;
+            }
+        } else {
+            for j in start_col_idx..=end_col_idx {
+                if is_symbol(line.chars().nth(j).unwrap()) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    false
+}
+
+fn solve(txt: &str) -> usize {
+    let lines: Vec<&str> = txt.lines().collect();
+    let mut sum = 0;
+
+    for (i, line) in lines.iter().enumerate() {
+        if line.is_empty() {
+            continue;
+        }
+        let ranges = get_number_ranges(line);
+        println!("{:?}", ranges);
+        for range in ranges {
+            if is_part_number(&lines, i, &range) {
+                sum += &line[range.clone()].parse::<usize>().unwrap();
+                println!("line:{:?}, range: {:?}, sum: {:?}", &i, &range, &sum);
+            }
+        }
+    }
+
     sum
 }
 
-fn main() {
-    let mut ans1: u32 = 0;
-    if let Ok(lines) = read_lines("p1.txt") {
-        if let Ok(vecLines) = lines.collect::<Result<Vec<String>, _>>() {
-            ans1 = solve_p1(&vecLines);
+fn read_file(file_name: &str) -> String {
+    match std::fs::read_to_string(file_name) {
+        Ok(txt) => txt,
+        Err(_) => {
+            eprintln!("Error reading file: {}", file_name);
+            std::process::exit(1);
         }
     }
+}
 
-    println!("Part 1: {}", ans1);
+fn main() {
+    let txt = read_file("p1.txt");
+
+    println!("{}", solve(&txt));
 }
