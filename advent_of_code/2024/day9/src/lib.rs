02be96d6949de042c2f9c5d0d69908fc25047470
Ray <kjwsl@fatherslab.com>
2025-04-05 11:50:27 +0900

    update(AoC): solve AoC 2024 day 9 part 2

--- advent_of_code/2024/day9/src/lib.rs
+++ advent_of_code/2024/day9/src/lib.rs
@@ -69,9 +69,8 @@ impl Solution {
 
     pub fn part2(input: &str) -> i64 {
         let mut decoded_map = DecodedMap::decode(input).expect("Failed to decode input");
-        println!("Decoded map: {:#?}", decoded_map);
         decoded_map.defragment();
-        println!("Decoded map: {:#?}", decoded_map);
+        println!("Decoded map: {}", decoded_map.to_str_repr());
         decoded_map.checksum()
     }
 }
@@ -113,20 +112,27 @@ impl DecodedMap {
 
         let mut curr_id = 0;
         let mut is_id = true;
+        let mut idx = 0;
 
-        for (i, ch) in input.chars().enumerate() {
+        for ch in input.chars() {
             let count = match ch.to_digit(10) {
                 Some(count) => count as usize,
                 None => {
                     return None;
                 }
             };
-            if is_id {
-                files.push((curr_id, Chunk::new(i, count)));
-                curr_id += 1;
-            } else {
-                spaces.push(Chunk::new(i, count));
+
+            // No need to push empty files/spaces
+            if count > 0 {
+                if is_id {
+                    files.push((curr_id, Chunk::new(idx, count)));
+                    curr_id += 1;
+                } else {
+                    spaces.push(Chunk::new(idx, count));
+                }
             }
+
+            idx += count;
             is_id = !is_id;
         }
 
@@ -134,15 +140,14 @@ impl DecodedMap {
     }
 
     pub fn defragment(&mut self) {
+        // Sort by id
+        self.files.sort_unstable_by_key(|f| f.0);
+
         // Start from the back of the files
         for i in (0..self.files.len()).rev() {
             for j in 0..self.spaces.len() {
-                // move_file_to_space can remove number of spaces
-                if self.files.get(i).is_none()
-                    || self.spaces.get(j).is_none()
-                    || self.files[i].1.pos < self.spaces[j].pos
-                {
-                    break;
+                if self.spaces[j].pos >= self.files[i].1.pos {
+                    continue;
                 }
                 // If the space is big enough to contain the file
                 if self.spaces[j].len >= self.files[i].1.len {
@@ -151,6 +156,42 @@ impl DecodedMap {
                 }
             }
         }
+    }
+
+    pub fn checksum(&self) -> i64 {
+        self.files
+            .iter()
+            .map(|f| {
+                (0..f.1.len)
+                    .map(|i| (f.1.pos + i) as i64 * f.0 as i64)
+                    .sum::<i64>()
+            })
+            .sum()
+    }
+
+    pub fn to_str_repr(&self) -> String {
+        let n = self.files.iter().map(|f| f.1.len).sum::<usize>()
+            + self.spaces.iter().map(|s| s.len).sum::<usize>();
+        let mut result = vec![String::new(); n];
+
+        for file in &self.files {
+            let num_str = file.0.to_string();
+            let len = file.1.len;
+            let pos = file.1.pos;
+            for i in 0..len {
+                result[pos + i] = num_str.clone();
+            }
+        }
+
+        for space in &self.spaces {
+            let len = space.len;
+            let pos = space.pos;
+            for i in 0..len {
+                result[pos + i] = ".".to_string();
+            }
+        }
+
+        result.join(", ")
     }
 
     fn move_file_to_space(
@@ -176,39 +217,65 @@ impl DecodedMap {
                 return Err("No space big enough to accomodate file");
             }
             cmp::Ordering::Equal => {
-                let space = self.spaces.remove(space_idx);
+                let og_file_pos = file.1.pos;
                 file.1.pos = space.pos;
+                space.len = 0;
+                self.spaces.push(Chunk::new(og_file_pos, file.1.len));
             }
             cmp::Ordering::Less => {
-                file.1.pos = space.pos;
+                let og_space_pos = space.pos;
+
+                // Remove the front part of the space
                 space.pos += file.1.len;
                 space.len -= file.1.len;
+
+                // The spaces that the file originally occupied are now empty spaces
+                self.spaces.push(Chunk::new(file.1.pos, file.1.len));
+
+                // Move the file to the space
+                file.1.pos = og_space_pos;
             }
         }
-
-        self.files.sort_by(|a, b| a.1.pos.cmp(&b.1.pos));
 
         Ok(())
     }
 
-    pub fn checksum(&self) -> i64 {
-        self.files
-            .iter()
-            .enumerate()
-            .map(|(i, file)| i as i64 * file.0 as i64)
-            .sum()
+    fn sort_and_merge(&mut self) {
+        self.files.sort_unstable_by_key(|f| f.1.pos);
+
+        self.spaces.retain(|space| space.len > 0);
+
+        self.spaces.sort_unstable_by_key(|s| s.pos);
+
+        // Merge adjacent spaces
+        let mut i = 0;
+        while i < self.spaces.len().saturating_sub(1) {
+            let curr_end = self.spaces[i].pos + self.spaces[i].len;
+            let next_start = self.spaces[i + 1].pos;
+
+            if curr_end >= next_start {
+                let next_end = self.spaces[i + 1].pos + self.spaces[i + 1].len;
+                self.spaces[i].len = next_end.saturating_sub(self.spaces[i].pos);
+                self.spaces.remove(i + 1);
+            } else {
+                i += 1;
+            }
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    const SAMPLE: &str = "2333133121414131402";
 
     #[test]
-    fn test_sample1() {
-        let sample = "12345";
-        // "0..111....22222"
-        // 022111222
-        assert_eq!(Solution::part1(sample), 1928);
+    fn test_part1() {
+        assert_eq!(Solution::part1(SAMPLE), 1928);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(Solution::part2(SAMPLE), 2858);
     }
 }
